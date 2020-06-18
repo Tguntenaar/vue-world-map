dfbec7e6aff0e8f655b78899e31df2bd323d2b50
T guntenaar <thomas@guntenaar.org>
2020-06-18 16:03:17 +0200

    Skips aanpassingen
    added moods

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,6 @@
 <script>
 import chroma from 'chroma-js';
 import Map from './Map';
-import {
-  getDynamicMapCss,
-  getBaseCss,
-  getCombinedCssString,
-} from './dynamic-map-css';
-
 
 export default {
   components: { Map },
@@ -22,14 +16,6 @@ export default {
     },
   },
   props: {
-    lowColor: {
-      type: String,
-      default: '#fde2e2',
-    },
-    highColor: {
-      type: String,
-      default: '#d83737',
-    },
     countryData: {
       type: Object,
       required: true,
@@ -46,13 +32,89 @@ export default {
   data() {
     return {
       node: document.createElement('style'),
-      chromaScale: chroma.scale([this.$props.lowColor, this.$props.highColor]),
     };
   },
   methods: {
     renderMapCSS() {
-      const baseCss = getBaseCss(this.$props);
-      const dynamicMapCss = getDynamicMapCss(this.$props.countryData, this.chromaScale);
+      const baseCss = `.vue-world-map .land{fill: \
+                       ${this.$props.defaultCountryFillColor}; \
+                       stroke:${this.$props.countryStrokeColor};}`
+
+      var happyMin, happyMax, sadMin, sadMax, angryMin,
+          angryMax, relaxedMin, relaxedMax;
+
+      // Determine the upper- and lower-bounds of every mood.
+      for (let [,[moodIntensity, mood]] of
+           Object.entries(this.$props.countryData)) {
+        switch (mood) {
+          case 0: // Happy.
+            if (moodIntensity < happyMin || happyMin === undefined)
+                happyMin = moodIntensity;
+            if (moodIntensity > happyMax || happyMax === undefined)
+                happyMax = moodIntensity;
+            break;
+          case 1: // Sad.
+            if (moodIntensity < sadMin || sadMin === undefined)
+                sadMin = moodIntensity;
+            if (moodIntensity > sadMax || sadMax === undefined)
+                sadMax = moodIntensity;
+            break;
+          case 2: // Angry.
+            if (moodIntensity < angryMin || angryMin === undefined)
+                angryMin = moodIntensity;
+            if (moodIntensity > angryMax || angryMax === undefined)
+                angryMax = moodIntensity;
+            break;
+          case 3: // Relaxed.
+            if (moodIntensity < relaxedMin || relaxedMin === undefined)
+                relaxedMin = moodIntensity;
+            if (moodIntensity > relaxedMax || relaxedMax === undefined)
+                relaxedMax = moodIntensity;
+            break;
+        }
+      }
+
+      const dynamicMapCss = [];
+      const happyChroma = chroma.scale(['#fffed4', '#f0ec00']),
+            sadChroma = chroma.scale(['#e9e6ff', '#1e00ff']),
+            angryChroma = chroma.scale(['#fde2e2', '#d83737']),
+            relaxedChroma = chroma.scale(['#e2fae1', '#04e600']);
+
+      // Set the css values of every country.
+      for (let [country, [moodIntensity, mood]] of
+           Object.entries(this.$props.countryData)) {
+        var hex = '#dadada'
+        switch (mood) {
+          case 0: // Happy.
+            var happyScaleValue = 1 / (happyMax - happyMin) *
+                                    (moodIntensity - happyMin);
+            hex = happyChroma(happyScaleValue).hex();
+            break;
+          case 1: // Sad.
+            var sadScaleValue = 1 / (sadMax - sadMin) *
+                                  (moodIntensity - sadMin);
+            hex = sadChroma(sadScaleValue).hex();
+            break;
+          case 2: // Angry.
+            var angryScaleValue = 1 / (angryMax - angryMin) *
+                                    (moodIntensity - angryMin);
+            hex = angryChroma(angryScaleValue).hex();
+            break;
+          case 3: // Relaxed.
+            var relaxedScaleValue = 1 / (relaxedMax - relaxedMin) *
+                                      (moodIntensity - relaxedMin);
+            hex = relaxedChroma(relaxedScaleValue).hex();
+            break;
+        }
+        dynamicMapCss.push(`.vue-world-map #${country} { fill: ${hex}; }`);
+      }
+
+      // Combine the country base colors with the dynamically assigned colors.
+      function getCombinedCssString(baseCss, dynamicCss) {
+        dynamicCss.push(baseCss);
+        return dynamicCss.join(' ');
+      }
+
       this.$data.node.innerHTML = getCombinedCssString(baseCss, dynamicMapCss);
     },
   },
